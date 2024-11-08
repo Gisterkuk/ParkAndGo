4d8e5036f31f92523a842451fb0bfeacfe781ccd
Gisterkuk <vegaalanadrian@gmail.com>
2024-11-08 01:21:11 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 dotenv.config();
 console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME_USER:", process.env.DB_NAME_USER);
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("PORT:", process.env.PORT);
 
